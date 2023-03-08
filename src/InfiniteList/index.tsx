058198d4625d9e7dddd97a95a403b903fb7e8bca
kevinthoriya <kevinthoriya0074@gmail.com>
2023-03-08 18:59:23 +0530

    [CleanUp] clean up fetch request
    - cleaned up fetch data request by aborting the fetch call
    - added unique key to each list and item of that list

--- src/InfiniteList/index.tsx
+++ src/InfiniteList/index.tsx
@@ -1,23 +1,29 @@
 import "./InfiniteList.css";
 
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useId, useState } from "react";
 
 import { Comment } from "../types";
 import CommentItem from "./ListItem";
 
 function InfiniteList() {
   const [data, setData] = useState<Comment[]>([]);
+  const uniqueListId = useId();
 
-  const getData = useCallback(async () => {
+  const getData = useCallback(async (signal: AbortSignal) => {
     let response = await fetch(
-      "https://jsonplaceholder.typicode.com/comments?_page=1&_limit=10"
+      "https://jsonplaceholder.typicode.com/comments?_page=1&_limit=10",
+      {
+        signal: signal,
+      }
     );
     let result = (await response.json()) as Comment[];
     setData((prev) => prev.concat(result));
   }, []);
 
   useEffect(() => {
-    getData();
+    let controller = new AbortController();
+    getData(controller.signal);
+    return () => controller.abort();
   }, [getData]);
 
   return (
@@ -25,7 +31,10 @@ function InfiniteList() {
       <h1>Infinite List</h1>
       <div className="">
         {data.map((comment) => (
-          <CommentItem comment={comment} />
+          <CommentItem
+            key={`${uniqueListId}-${comment.id}`}
+            comment={comment}
+          />
         ))}
       </div>
     </div>
