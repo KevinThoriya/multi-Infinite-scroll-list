65791962055866bf0002a5ce481afc6ffb0de26c
kevinthoriya <kevinthoriya0074@gmail.com>
2023-03-08 18:06:15 +0530

    [Add] component for infinite list
    - added a empty component for infinite list
    - display multiple infinite list on main container.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import './App.css'
+import "./App.css";
+
+import InfiniteList from "./InfiniteList";
 
 function App() {
-
   return (
     <div className="App">
-       <h1>Multi Infinite Scroll List</h1>
+      <h1>Multi Infinite Scroll List</h1>
+      <div className="parentContainer">
+        <InfiniteList />
+        <InfiniteList />
+        <InfiniteList />
+        <InfiniteList />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/InfiniteList/index.tsx
@@ -0,0 +1,15 @@
+import './InfiniteList.css'
+
+function InfiniteList() {
+
+  return (
+    <div className="listContainer">
+       <h1>Infinite List</h1>
+       <div className="">
+          
+       </div>
+    </div>
+  )
+}
+
+export default InfiniteList
